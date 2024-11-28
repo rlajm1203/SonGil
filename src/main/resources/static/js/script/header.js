103e331a5845ecf3cfb953b35fe722766c63a676
bellmin <whdlas011203@naver.com>
2024-11-28 22:15:23 +0900

    feat: 로그아웃 모달 구현

--- src/main/resources/static/js/script/header.js
+++ src/main/resources/static/js/script/header.js
@@ -1,17 +1,27 @@
-// 모달 열기
-function openModal() {
-    document.getElementById('logout-modal').style.display = 'block';
+// 모달 표시/숨기기 토글 함수
+function toggleModal() {
+    const modal = document.getElementById('logout-modal');
+    if (modal.style.display === 'none' || modal.style.display === '') {
+        modal.style.display = 'block';
+    } else {
+        modal.style.display = 'none';
+    }
 }
 
-// 모달 닫기
-function closeModal() {
-    document.getElementById('logout-modal').style.display = 'none';
-}
-
-// 로그아웃
+// 로그아웃 함수
 function logout() {
+    // 로컬 스토리지에서 토큰 제거
     localStorage.removeItem('accessToken');
     localStorage.removeItem('accessTokenExpiration');
     alert('로그아웃 되었습니다.');
-    location.reload();
+    location.reload(); // 페이지 새로고침
 }
+
+// 페이지 어디를 클릭해도 모달이 닫히도록
+document.addEventListener('click', (event) => {
+    const modal = document.getElementById('logout-modal');
+    const userName = document.getElementById('member-name');
+    if (modal.style.display === 'block' && !modal.contains(event.target) && event.target !== userName) {
+        modal.style.display = 'none';
+    }
+});
