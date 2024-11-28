54fcf33544d6a3d46b26f7182f5c3bf5633f00fa
bellmin <whdlas011203@naver.com>
2024-11-28 17:18:56 +0900

    feat: 액세스 토큰 저장 구현 및 로그아웃 구현

--- src/main/resources/static/js/script/login-script.js
+++ src/main/resources/static/js/script/login-script.js
@@ -14,14 +14,22 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         body: JSON.stringify(loginData)
     })
         .then(response => {
+
             if (!response.ok) throw new Error('로그인 실패');
             return response.json();
         })
         .then(data => {
-            if (data.accessToken) {
-                localStorage.setItem('accessToken', data.accessToken);
-                alert('로그인 성공!');
-                window.location.href = '/view/index';
+            // 'data' 객체 내부에 접근
+            const accessToken = data.data.accessToken; // accessToken 추출
+            const accessExpiredTime = data.data.accessExpiredTime; // 만료 시간 추출
+            const message = data.message; // 성공 메시지 추출
+
+            // Access Token 저장
+            if (accessToken) {
+                localStorage.setItem('accessToken', accessToken);
+                localStorage.setItem('accessExpiredTime', accessExpiredTime);
+                alert('로그인 성공: ' + message);
+                window.location.href = '/view/index'; // 성공 후 리다이렉션
             } else {
                 throw new Error('Access Token이 없습니다.');
             }
