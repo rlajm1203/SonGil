aa843d5e3e03d10e01c09b4ac7a99d7d46c5c3c7
bellmin <whdlas011203@naver.com>
2024-11-30 17:17:50 +0900

    feat: WebConfig 추가

---
+++ src/main/java/com/jnu/mcd/ddobagi/common/config/WebConfig.java
@@ -0,0 +1,43 @@
+package com.jnu.mcd.ddobagi.common.config;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;ㅇ
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE_SECS = 3600;
+    private final List<String> allowOriginUrlPatterns;
+    public static final String ALLOWED_METHOD_NAMES =
+            "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+
+
+
+    public WebConfig(
+            @Value("${cors.allow-origin.urls}") final String allowOriginUrlPatterns) {
+        this.allowOriginUrlPatterns =
+                Arrays.stream(allowOriginUrlPatterns.split(","))
+                        .map(String::trim)
+                        .collect(Collectors.toList());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] patterns = allowOriginUrlPatterns.toArray(String[]::new);
+
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns(patterns)
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders("Authorization", "Set-Cookie")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
